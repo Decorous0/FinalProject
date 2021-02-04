f72ae5d0acc40f53ed7804b5d15fcbe03b46f00c
Decorous0 <amateurinstallers@gmail.com>
2021-02-04 21:23:42 +0300

    Final

--- DataAccess/Abstract/IEntityRespository.cs
+++ DataAccess/Abstract/IEntityRespository.cs
@@ -1,11 +1,12 @@
-﻿using System;
+﻿using Entities.Abstract;
+using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface IEntityRespository<T>
+    public interface IEntityRespository<T> where T:class,IEntity
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
