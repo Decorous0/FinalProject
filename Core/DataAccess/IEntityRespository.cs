e1d6d4aa2920bed3e9482cb77c3e57506e7321be
Decorous0 <amateurinstallers@gmail.com>
2021-02-07 04:14:36 +0300

    Final

--- Core/DataAccess/IEntityRespository.cs
+++ Core/DataAccess/IEntityRespository.cs
@@ -1,12 +1,13 @@
-﻿using Entities.Abstract;
+﻿
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAcces
 {
-    public interface IEntityRespository<T> where T:class,IEntity
+    public interface IEntityRespository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
         T Get(Expression<Func<T, bool>> filter);
--- DataAccess/Abstract/ICustomerDal.cs
+++ DataAccess/Abstract/ICustomerDal.cs
@@ -1,4 +1,5 @@
 ﻿
+using Core.DataAcces;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
