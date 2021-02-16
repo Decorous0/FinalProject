8ec3f36ab5b2bb89ecc4d2bcaf125ef5b30fac61
Decorous0 <amateurinstallers@gmail.com>
2021-02-16 08:55:09 +0300

    DERS

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -22,6 +22,7 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
+          
             //business codes
 
             if (product.ProductName.Length<2)
@@ -36,7 +37,7 @@ namespace Business.Concrete
 
         public IDataResult<List<Product>> GetAll()
         {
-            if (DateTime.Now.Hour==22)
+            if (DateTime.Now.Hour == 10)
             {
                 return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
             }
